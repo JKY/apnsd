e32e4fd6d47b170d24c6416ec0115446fae00908
JKY <yanghf1985@hotmail.com>
2015-11-08 18:10:30 +0800

    updated

--- tests/foo.php
+++ tests/foo.php
@@ -1,12 +1,18 @@
 <?php
+/* make sure you have installed the peb extation */
 require(dirname(__FILE__). '/../if/apns.lib.php');
-/* 
- * connect service, with cname: apnsd, cookie: 123 
- * NOTES: the cname and cookie is the same with erlang start command 
- */
-$agent = new APNSAgent("apnsd@127.0.0.1","123");
-/* 
- * push message to channel foo, device: dev 
- */
-echo $agent->push("foo", "dev", "hello world....",array());
+if(sizeof($argv) != 4){
+	echo "usage: php foo.php ch dev message";
+}else{
+	/* 
+ 	 * connect service, with cname: apnsd, cookie: 123 
+ 	 * NOTES: the cname and cookie is the same with erlang start command 
+ 	 */
+	$agent = new APNSAgent("apnsd@127.0.0.1","123");
+	/* 
+     * push message to channel foo, device: dev 
+     */
+	echo $agent->push($argv[1], $argv[2], $argv[3],array());
+}
+echo "\n";
 ?>
